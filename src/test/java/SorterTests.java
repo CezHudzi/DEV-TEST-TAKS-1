3825c4244f7326fe9b8d52c45fe9e06ec0f97845
cezary.hudzik <cezary.hudzik@asseco.pl>
2020-12-08 17:26:07 +0100

    simple sorter test

---
+++ src/test/java/SorterTests.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+import services.Sorter;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+
+public class SorterTests {
+
+    @Test
+    public void quickSortTest_correctUnsortedInput_correctSortedOutput() {
+        Sorter sorter = new Sorter();
+        ArrayList<Integer> unsortedList1 = new ArrayList<>(
+                Arrays.asList(1, 10, 20, 20, 2, 5));
+        ArrayList<Integer> sortedList1 = new ArrayList<>(
+                Arrays.asList(1, 2, 5, 10, 20, 20));
+
+        // assert statements
+        assertEquals(unsortedList1, sorter.quickSort(sortedList1));
+
+    }
+}
