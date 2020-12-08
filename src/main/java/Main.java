7350232946cd261ba27857f3d204c025b19de1af
cezary.hudzik <cezary.hudzik@asseco.pl>
2020-12-08 22:48:21 +0100

    preered output informations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,23 @@
-import java.util.ArrayList;
+import services.Calculator;
 import java.util.Arrays;
-import static services.NumberInput.readNumsFromCommandLine;
+import static services.Converter.StringArrToIntArr;
 
 public class Main {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
-        ArrayList<Integer> numbers = readNumsFromCommandLine();
-        numbers.forEach(number -> System.out.println(number.toString()));
+        Calculator calculator = new Calculator();
+        int[] input = StringArrToIntArr(args);
+
+        int[] resault = calculator.quickSort(input);
+
+        Arrays.stream(resault).forEach(number -> System.out.print(number + " "));
+        System.out.println();
+        System.out.println("count: " + args.length);
+        System.out.println("distinct: " + calculator.distinct(input));
+        System.out.println("min: " + resault[0]);
+        System.out.println("max: " + resault[resault.length-1]);
+
     }
 
 }
