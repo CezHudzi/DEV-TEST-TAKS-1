1d93b4330843fe455e2727ba8b9ad48cac8db0bc
cezary.hudzik <cezary.hudzik@asseco.pl>
2020-12-08 22:45:16 +0100

    added tests for distinct method

--- src/test/java/CalculatorTests.java
+++ src/test/java/CalculatorTests.java
@@ -15,5 +15,13 @@ public class CalculatorTests {
         Assert.assertArrayEquals(sortedList1, calculator.quickSort(unsortedList1));
     }
 
+    @Test
+    public void distinctTest_correctInput_correctNumberOfDistinct() {
+        Calculator calculator = new Calculator();
+        int[] distinct1 = {1, 10, 20, 20, 2, 5, 8, 8, 8, 8, 5, 1, 5};
+
+        // assert statements
+        Assert.assertEquals(6, calculator.distinct(distinct1));
+    }
 
 }
